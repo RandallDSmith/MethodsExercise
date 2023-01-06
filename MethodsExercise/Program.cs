79e261a651d9f87f172795354e8033f73ffb3242
Randall Smith <rdsmith1761@gmail.com>
2023-01-05 19:39:05 -0500

    Exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Exercise 1
+            Console.WriteLine("Hello, What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName} What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is a great color.  What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"{userName}'s {color} {animal} likes to listen to {band}");
+
+
+
         }
     }
 }
